2b130c32fd8f2da823bfcac56308dca578db9e97
gk <grzegorz.korcyl@gmail.com>
2015-11-19 15:08:16 +0100

    update

--- hdl/soda/dc_sources/dc_trb_tdc/tb_cts_soda_trigger.vhd
+++ hdl/soda/dc_sources/dc_trb_tdc/tb_cts_soda_trigger.vhd
@@ -55,12 +55,12 @@ begin
 		generic map(
 			EXTERNAL_TRIGGER_ID  => x"60", -- fill in trigger logic enumeration id of external trigger logic
 
-			TRIGGER_COIN_COUNT   => 1, --TRIGGER_COIN_COUNT,
+			TRIGGER_COIN_COUNT   => 0, --TRIGGER_COIN_COUNT,
 			TRIGGER_PULSER_COUNT => 2, --TRIGGER_PULSER_COUNT,
-			TRIGGER_RAND_PULSER  => 1, --TRIGGER_RAND_PULSER,
-			TRIGGER_INPUT_COUNT  => 1,  -- obsolete! now all inputs are routed via an input multiplexer!
+			TRIGGER_RAND_PULSER  => 0, --TRIGGER_RAND_PULSER,
+			TRIGGER_INPUT_COUNT  => 0,  -- obsolete! now all inputs are routed via an input multiplexer!
 			TRIGGER_ADDON_COUNT  => 1, --TRIGGER_ADDON_COUNT,
-			PERIPH_TRIGGER_COUNT => 1, --PERIPH_TRIGGER_COUNT,
+			PERIPH_TRIGGER_COUNT => 0, --PERIPH_TRIGGER_COUNT,
 			OUTPUT_MULTIPLEXERS  => 0, --CTS_OUTPUT_MULTIPLEXERS,
 			ADDON_LINE_COUNT     => 38, --CTS_ADDON_LINE_COUNT,
 			ADDON_GROUPS         => 7,
