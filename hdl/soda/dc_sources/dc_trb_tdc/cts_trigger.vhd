87a733bbe1960aa926b18a1f7ca468d62314cb86
gk <grzegorz.korcyl@gmail.com>
2015-11-19 15:23:17 +0100

    update

--- hdl/soda/dc_sources/dc_trb_tdc/cts_trigger.vhd
+++ hdl/soda/dc_sources/dc_trb_tdc/cts_trigger.vhd
@@ -424,8 +424,8 @@ begin
             channel_mask_i <= (others => '0');
             channel_edge_select_i <= (others => '1');
             
-            trigger_input_configs_i <= (others => (others => '1'));
-            coin_config_i <= (others => X"000F0000");
+            trigger_input_configs_i <= (others => (others => '0'));
+            coin_config_i <= (others => X"ffffffff");
             --pulser_interval_i <= (1 => X"00000003",  others => (others => '1'));
             pulser_interval_i <= (others => (others => '1'));
             
