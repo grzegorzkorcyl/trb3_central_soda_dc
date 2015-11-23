8212a30a6345a04238562cfcb877cd826bea5adb
gk <grzegorz.korcyl@gmail.com>
2015-11-23 16:12:23 +0100

    update

--- hdl/soda/dc_sources/dc_trb_tdc/dc_module_trb_tdc.vhd
+++ hdl/soda/dc_sources/dc_trb_tdc/dc_module_trb_tdc.vhd
@@ -207,14 +207,14 @@ begin
 				save_next_state <= ADD_SUBSUB4;
 
 			when ADD_SUBSUB4 =>
-				if (save_ctr /= "11") then
+				if (save_ctr(1 downto 0) /= "11") then
 					save_next_state <= SAVE_PADDING;
 				else
 					save_next_state <= CLEANUP;
 				end if;
 				
 			when SAVE_PADDING =>
-				if (save_ctr = "11") then
+				if (save_ctr(1 downto 0) = "11") then
 					save_next_state <= CLEANUP;
 				else
 					save_next_state <= SAVE_PADDING;
@@ -256,7 +256,7 @@ begin
 
 				when ADD_SUBSUB4 =>
 					sf_data  <= FEE_STATUS_BITS_IN(15 downto 0);
-					if (save_ctr = "11") then
+					if (save_ctr(1 downto 0) = "11") then
 						save_eod <= '1';
 					else
 						save_eod <= '0';
@@ -264,7 +264,7 @@ begin
 					
 				when SAVE_PADDING =>
 					sf_data <= x"abcd";
-					if (save_ctr = "11") then
+					if (save_ctr(1 downto 0) = "11") then
 						save_eod <= '1';
 					else
 						save_eod <= '0';
