69361cd981543c83334912380523b0bcc1c603fc
gk <grzegorz.korcyl@gmail.com>
2015-12-01 14:46:17 +0100

    update

--- hdl/soda/dc_sources/dc_trb_tdc/cts_trigger.vhd
+++ hdl/soda/dc_sources/dc_trb_tdc/cts_trigger.vhd
@@ -426,7 +426,7 @@ begin
             
             trigger_input_configs_i <= (others => (others => '0'));
             coin_config_i <= (others => X"000F0001");
-            pulser_interval_i <= (0 => x"0000ffff", others => (others => '1'));
+            pulser_interval_i <= (0 => x"000000ff", others => (others => '1'));
             
             rand_pulser_threshold_i <= (others => (others => '0'));
 
